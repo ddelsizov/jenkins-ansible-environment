88c6830e1e0c55dc4ec860baa593caf87da1ce05
ddelsizov <49860259+ddelsizov@users.noreply.github.com>
2021-06-22 03:22:38 +0300

    Add files via upload

--- credential.sh
+++ credential.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "** Sleep for 60 seconds until Jenkins is UP after restart"
+sudo sleep 60
+echo "** Execute create credential"
 sudo cat /vagrant/credential.xml | sudo java -jar /opt/jenkins-cli.jar -s http://localhost:8080 -auth ddelsizov:secretpassword create-credentials-by-xml "SystemCredentialsProvider::SystemContextResolver::jenkins" "(global)"
