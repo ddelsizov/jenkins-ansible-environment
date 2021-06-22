1757c64a538725d1a08b7099bc42f16d1df0301c
ddelsizov <49860259+ddelsizov@users.noreply.github.com>
2021-06-22 20:42:33 +0300

    Add files via upload

--- slaveconf.sh
+++ slaveconf.sh
@@ -4,7 +4,7 @@ sudo useradd jenkins
 sudo echo "jenkins:secretpassword" | sudo chpasswd
 sudo echo 'jenkins ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 sudo mkdir -p /opt/jenkins
-sudo chmod jenkins:jenkins -R /opt/jenkins
+sudo chown jenkins:jenkins -R /opt/jenkins
 
 echo "*** Add hosts"
 echo "192.168.99.100 jenkins.home.lab jenkins" >> /etc/hosts
@@ -12,7 +12,8 @@ echo "192.168.99.101 slave.home.lab slave" >> /etc/hosts
 
 echo "***Install Java  and additional python libs"
 sudo dnf install -y java-11-openjdk-devel
-sudo dnf install -y python3-libselinux python3-policycoreutils python3-libsemanage
+sudo dnf install python3 -y
+sudo dnf install python3-libselinux -y
 
 echo "*** Add hosts, open firewall"
 
