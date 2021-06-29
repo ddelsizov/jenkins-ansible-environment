8317974f11ab8f58229be7010a51a39af6887f09
ddelsizov <49860259+ddelsizov@users.noreply.github.com>
2021-06-29 22:37:22 +0300

    Add files via upload

--- vagrant/jenkins/master/createjob.sh
+++ vagrant/jenkins/master/createjob.sh
@@ -10,7 +10,7 @@ cat <<EOF | java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ -http -auth
   <keepDependencies>false</keepDependencies>
   <properties>
     <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github">
-      <projectUrl>https://github.com/ddelsizov/basic-docker-poc</projectUrl>
+      <projectUrl>https://github.com/ddelsizov/basic-docker-poc.git</projectUrl>
       <displayName>Test</displayName>
     </com.coravy.hudson.plugins.github.GithubProjectProperty>
   </properties>
@@ -18,7 +18,7 @@ cat <<EOF | java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ -http -auth
     <configVersion>2</configVersion>
     <userRemoteConfigs>
       <hudson.plugins.git.UserRemoteConfig>
-        <url>https://github.com/ddelsizov/basic-docker-poc</url>
+        <url>https://github.com/ddelsizov/basic-docker-poc.git</url>
       </hudson.plugins.git.UserRemoteConfig>
     </userRemoteConfigs>
     <branches>
@@ -37,18 +37,20 @@ cat <<EOF | java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ -http -auth
   <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
   <triggers>
     <hudson.triggers.SCMTrigger>
-      <spec>5 * * * *</spec>
+      <spec>H/2 * * * *</spec>
       <ignorePostCommitHooks>false</ignorePostCommitHooks>
     </hudson.triggers.SCMTrigger>
   </triggers>
   <concurrentBuild>false</concurrentBuild>
   <customWorkspace>/opt/jenkins/projects/</customWorkspace>
   <builders>
+  <hudson.tasks.Shell>
+   <command>
+#!/bin/bash -l /vagrant/cleanup.sh
+   </command>
+  </hudson.tasks.Shell>
     <hudson.tasks.Shell>
-      <command>
-#!/bin/bash -l
-/vagrant/cleanup.sh \
-git clone https://github.com/ddelsizov/basic-docker-poc /opt/jenkins/projects/docker \
+ <command>
 docker image build -t apache-1 /opt/jenkins/projects/docker/
 </command>
     </hudson.tasks.Shell>
@@ -57,8 +59,7 @@ docker image build -t apache-1 /opt/jenkins/projects/docker/
     </hudson.tasks.Shell>
 	<hudson.tasks.Shell>
       <command>
-#!/bin/bash -l
-/vagrant/test.sh
+#!/bin/bash -l /vagrant/test.sh
 	  </command>
     </hudson.tasks.Shell>
   </builders>
