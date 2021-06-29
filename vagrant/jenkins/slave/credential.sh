60140e7843795e9fa90c4e2cc28e6add9824f12f
ddelsizov <49860259+ddelsizov@users.noreply.github.com>
2021-06-29 20:53:07 +0300

    Delete credential.sh

--- vagrant/jenkins/slave/credential.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "** Sleep for 60 seconds until Jenkins is UP after restart"
-sudo sleep 60
-echo "** Execute create credential"
-sudo cat /vagrant/jenkins/credential.xml | sudo java -jar /opt/jenkins-cli.jar -s http://localhost:8080 -auth ddelsizov:secretpassword create-credentials-by-xml "SystemCredentialsProvider::SystemContextResolver::jenkins" "(global)"
