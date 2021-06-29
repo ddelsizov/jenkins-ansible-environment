78eecaa105a53509534b2a0ea5a870b0fced2fa3
ddelsizov <49860259+ddelsizov@users.noreply.github.com>
2021-06-29 23:20:27 +0300

    Add files via upload

--- vagrant/jenkins/slave/cleanup.sh
+++ vagrant/jenkins/slave/cleanup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "Clean up containers"
 stop_container() {
   if [ "$(docker ps -aq -f status=running -f name=apache-web)" ]; then
-      echo -ne "Stop "
+      echo -ne "Kill apache-web "
       #docker stop $1
       docker kill apache-web
+	  docker rm apache-web
   fi
 
   if [ "$(docker ps -aq -f status=exited -f name=apache-web)" ]; then
--- vagrant/jenkins/slave/test.sh
+++ vagrant/jenkins/slave/test.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -l
-options=\'-o /dev/null -w %{http_code} -sfI\'
+#!/bin/bash
+options='-o /dev/null -w %{http_code} -sfI'
 page="http://localhost:80"
-outstr=$(sudo curl $options $page)
+sleep 30
+outstr=$(curl $options $page)
 retVal=$?
 [[ $retVal -eq 0 ]] || { echo "ERROR should have been able to pull $page, retVal=$retVal, code=$outstr"; exit 4; }
 echo "OK pulling from $page successful, retVal=$retVal, code=$outstr"
