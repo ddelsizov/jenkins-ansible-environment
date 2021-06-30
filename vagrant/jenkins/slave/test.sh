198a17d9d906086a7038228712047e3cf20b9a2d
ddelsizov <49860259+ddelsizov@users.noreply.github.com>
2021-06-30 12:58:10 +0300

    Delete vagrant/jenkins directory

--- vagrant/jenkins/slave/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-options='-o /dev/null -w %{http_code} -sfI'
-page="http://localhost:80"
-sleep 30
-outstr=$(curl $options $page)
-retVal=$?
-[[ $retVal -eq 0 ]] || { echo "ERROR should have been able to pull $page, retVal=$retVal, code=$outstr"; exit 4; }
-echo "OK pulling from $page successful, retVal=$retVal, code=$outstr"
